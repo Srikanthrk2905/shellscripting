330849c9764960fc40cb2e1a5f0fd896bc8b4a79
srikanthrk <srikanthrkdevops@gmail.com>
2022-07-20 19:23:59 +0530

    Frontend automation setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+StatCheck_() {
+if [ $1 -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+  exit 2
+fi
+}
 
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ];then
@@ -8,59 +15,26 @@ if [ "$USER_ID" -ne 0 ];then
 fi
 
 echo -e "\e[36m Installing Nginx \e[0m"
-
 yum install nginx -y
-
-STATUS_() {
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
-}
-
-STATUS
+StatCheck $1
 echo -e "\e[36m download Nginx \e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-STATUS
-
+StatCheck $1
 echo -e "\e[36m Clean old and download new Nginx Arcive \e[0m"
 rm -rf /usr/share/nginx/html/*
-
-STATUS
-
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-STATUS
-
+StatCheck $1
 echo -e "\e[36m restart Nginx \e[0m"
 systemctl restart nginx
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+StatCheck $1
 echo -e "\e[36m Enabling Nginx \e[0m"
 systemctl enable nginx
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+StatCheck $1
 echo -e "\e[36m Starting Nginx \e[0m"
 systemctl start nginx
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
-
-}
+StatCheck $1
