7e8c3800141f2334cc7138c83a607887ab882d93
srikanthrk <srikanthrkdevops@gmail.com>
2022-07-20 19:16:23 +0530

    Frontend automation setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -23,22 +23,12 @@ fi
 STATUS
 echo -e "\e[36m download Nginx \e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+STATUS
 
 echo -e "\e[36m Clean old and download new Nginx Arcive \e[0m"
 rm -rf /usr/share/nginx/html/*
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+STATUS
 
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
@@ -46,12 +36,7 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
+STATUS
 
 echo -e "\e[36m restart Nginx \e[0m"
 systemctl restart nginx
