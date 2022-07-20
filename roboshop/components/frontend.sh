d574dded4975f40521576a3277047bda7eaad60e
srikanthrk <srikanthrkdevops@gmail.com>
2022-07-20 17:32:53 +0530

    Frontend automation setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,7 +7,7 @@ if [ "$USER_ID" -ne 0 ];then
 fi
 
 yum install nginx -y
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -15,7 +15,7 @@ else
 fi
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -23,7 +23,8 @@ else
 fi
 
 rm -rf /usr/share/nginx/html/*
-if [$? -eq 0]; then
+
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -31,7 +32,7 @@ else
 fi
 
 cd /usr/share/nginx/html/
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -39,35 +40,35 @@ else
 fi
 
 unzip /tmp/frontend.zip
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
   exit 1
 fi
 mv frontend-main/* .
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
   exit 1
 fi
 mv static/* .
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
   exit 1
 fi
 rm -rf frontend-main README.md
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
   exit 1
 fi
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -75,21 +76,21 @@ else
 fi
 
 systemctl restart nginx
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
   exit 1
 fi
 systemctl enable nginx
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
   exit 1
 fi
 systemctl start nginx
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
