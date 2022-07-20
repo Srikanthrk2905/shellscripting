5cdebae3ff972497b5ad1bb9c8fddfb168a36d1d
srikanthrk <srikanthrkdevops@gmail.com>
2022-07-20 19:28:13 +0530

    Frontend automation setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -16,10 +16,10 @@ fi
 
 echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
-StatCheck $1
+StatCheck $?
 echo -e "\e[36m download Nginx \e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-StatCheck $1
+StatCheck $?
 echo -e "\e[36m Clean old and download new Nginx Arcive \e[0m"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html/
@@ -28,13 +28,13 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-StatCheck $1
+StatCheck $?
 echo -e "\e[36m restart Nginx \e[0m"
 systemctl restart nginx
-StatCheck $1
+StatCheck $?
 echo -e "\e[36m Enabling Nginx \e[0m"
 systemctl enable nginx
-StatCheck $1
+StatCheck $?
 echo -e "\e[36m Starting Nginx \e[0m"
 systemctl start nginx
-StatCheck $1
+StatCheck $?
