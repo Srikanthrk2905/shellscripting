6b83a7e28da17923b62ce9ffd5ffb561f59c83e4
srikanthrk <srikanthrkdevops@gmail.com>
2022-07-21 09:40:59 +0530

    Frontend automation setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,7 +7,8 @@ else
   exit 2
 fi
 }
-
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
 print() {
   echo -e "\e[36m $1 \e[0m"
 }
@@ -19,25 +20,25 @@ if [ "$USER_ID" -ne 0 ];then
 fi
 
 print "Installing Nginx"
-yum install nginx -y
+yum install nginx -y >>$LOG_FILE
 StatCheck $?
 
 print "download Nginx"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
 StatCheck $?
 
 print "Clean old Nginx"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 StatCheck $?
 
 cd /usr/share/nginx/html/
 
 print "extracting nginx"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
 StatCheck $?
 
 print "Update Roboshop configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
 StatCheck $?
 
 print "restart Nginx"
