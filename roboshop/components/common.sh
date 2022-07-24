cf7aa24feefdfb52eb8252419590c0deaad5597c
srikanthrk <srikanthrkdevops@gmail.com>
2022-07-24 19:22:03 +0530

    Mongodb automation setup

---
+++ roboshop/components/common.sh
@@ -0,0 +1,19 @@
+StatCheck() {
+if [ $1 -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+  exit 2
+fi
+}
+print() {
+  echo -e "\e[36m $1 \e[0m"
+}
+
+USER_ID=$(id -u)
+if [ "$USER_ID" -ne 0 ];then
+  echo you should be root user to run this command
+  exit 1
+fi
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-StatCheck() {
-if [ $1 -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-  exit 2
-fi
-}
-print() {
-  echo -e "\e[36m $1 \e[0m"
-}
-
-USER_ID=$(id -u)
-if [ "$USER_ID" -ne 0 ];then
-  echo you should be root user to run this command
-  exit 1
-fi
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
+source component/common. sh
 
 print "setup yum repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
