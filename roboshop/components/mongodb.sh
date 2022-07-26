800e7d35d05df5b72de9b4a19c39c736c0760b0a
srikanthrk <srikanthrkdevops@gmail.com>
2022-07-26 16:57:13 +0530

    Mongodb automation setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -19,7 +19,7 @@ systemctl enable mongod && systemctl start mongod
 StatCheck $?
 
 print "download schema"
-curl -f -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
 StatCheck $?
 
 print "extract schema"
